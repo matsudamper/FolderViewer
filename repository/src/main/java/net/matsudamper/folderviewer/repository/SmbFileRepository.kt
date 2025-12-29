7b30c2511b887c693223fe37ad58c69f46e91424
matsudamper <let@matsudamper.net>
2025-12-30 06:27:44 +0900

    format

--- repository/src/main/java/net/matsudamper/folderviewer/repository/SmbFileRepository.kt
+++ repository/src/main/java/net/matsudamper/folderviewer/repository/SmbFileRepository.kt
@@ -1,13 +1,6 @@
 package net.matsudamper.folderviewer.repository
 
-import com.hierynomus.msdtyp.AccessMask
-import com.hierynomus.mssmb2.SMB2CreateDisposition
-import com.hierynomus.mssmb2.SMB2ShareAccess
-import com.hierynomus.smbj.SMBClient
-import com.hierynomus.smbj.auth.AuthenticationContext
-import com.hierynomus.smbj.share.DiskShare
 import java.io.InputStream
-import java.util.EnumSet
 
 class SmbFileRepository : FileRepository {
     override suspend fun getFiles(path: String): List<FileItem> {
