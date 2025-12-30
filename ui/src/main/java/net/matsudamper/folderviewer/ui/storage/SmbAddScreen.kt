cd847b3f910f672f1282dc5d7ebf2a550149fbdb
matsudamper <let@matsudamper.net>
2025-12-30 09:48:04 +0900

    fix deprecated

--- ui/src/main/java/net/matsudamper/folderviewer/ui/storage/SmbAddScreen.kt
+++ ui/src/main/java/net/matsudamper/folderviewer/ui/storage/SmbAddScreen.kt
@@ -28,7 +28,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import net.matsudamper.folderviewer.ui.R
 
 @OptIn(ExperimentalMaterial3Api::class)
