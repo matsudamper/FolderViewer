7e85ed270b55886600e8f25d3c9c79e26db1d575
matsudamper <let@matsudamper.net>
2026-01-02 10:01:40 +0900

    未使用ファイルを削除

--- ui/src/main/java/net/matsudamper/folderviewer/ui/browser/FileImageSource.kt
+++
@@ -1,8 +0,0 @@
-package net.matsudamper.folderviewer.ui.browser
-
-public sealed interface FileImageSource {
-    public val path: String
-
-    public data class Thumbnail(override val path: String) : FileImageSource
-    public data class Original(override val path: String) : FileImageSource
-}
