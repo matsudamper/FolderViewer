9ce96a528a970f36cdb95ebf534849583511c58c
matsudamper <let@matsudamper.net>
2026-01-03 05:35:41 +0900

    表示を調整

--- ui/src/main/java/net/matsudamper/folderviewer/ui/browser/FileListItem.kt
+++ ui/src/main/java/net/matsudamper/folderviewer/ui/browser/FileListItem.kt
@@ -1,13 +1,17 @@
 package net.matsudamper.folderviewer.ui.browser
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -26,26 +30,33 @@ internal fun FileListItem(
     ListItem(
         modifier = modifier.clickable(onClick = onClick),
         leadingContent = {
-            val imageSource = file.imageSource
-            if (imageSource != null) {
-                AsyncImage(
-                    model = imageSource,
-                    contentDescription = null,
-                    imageLoader = imageLoader,
-                    modifier = Modifier
-                        .size(24.dp)
-                        .aspectRatio(1f),
-                    contentScale = ContentScale.Crop,
-                    placeholder = painterResource(R.drawable.ic_file),
-                    error = painterResource(R.drawable.ic_file),
-                )
-            } else {
-                Icon(
-                    painter = painterResource(
-                        id = if (file.isDirectory) R.drawable.ic_folder else R.drawable.ic_file,
-                    ),
-                    contentDescription = null,
-                )
+            Box(
+                modifier = Modifier
+                    .size(64.dp)
+                    .aspectRatio(1f),
+            ) {
+                val imageSource = file.imageSource
+                if (imageSource != null) {
+                    AsyncImage(
+                        model = imageSource,
+                        contentDescription = null,
+                        imageLoader = imageLoader,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .clip(MaterialTheme.shapes.small),
+                        contentScale = ContentScale.Crop,
+                        placeholder = painterResource(R.drawable.ic_file),
+                        error = painterResource(R.drawable.ic_file),
+                    )
+                } else {
+                    Icon(
+                        modifier = Modifier.fillMaxSize(),
+                        painter = painterResource(
+                            id = if (file.isDirectory) R.drawable.ic_folder else R.drawable.ic_file,
+                        ),
+                        contentDescription = null,
+                    )
+                }
             }
         },
         headlineContent = { Text(file.name) },
