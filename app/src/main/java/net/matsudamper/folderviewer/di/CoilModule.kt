82ccf7cbc7e2397c9cbfb4201d028836fad8305a
matsudamper <let@matsudamper.net>
2026-01-05 03:11:42 +0900

    fix import

--- app/src/main/java/net/matsudamper/folderviewer/di/CoilModule.kt
+++ app/src/main/java/net/matsudamper/folderviewer/di/CoilModule.kt
@@ -5,6 +5,7 @@ import coil.ImageLoader
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import jakarta.inject.Singleton
 import net.matsudamper.folderviewer.coil.CoilImageLoaderFactory
