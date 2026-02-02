b0dee388fdac0915e580d5c5a1155cb7914f64b9
matsudamper <let@matsudamper.net>
2026-02-03 03:50:01 +0900

    アイコンを変更

--- ui/src/main/java/net/matsudamper/folderviewer/ui/browser/FileBrowserScreenContent.kt
+++ ui/src/main/java/net/matsudamper/folderviewer/ui/browser/FileBrowserScreenContent.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.FloatingToolbarDefaults
+import androidx.compose.material3.FloatingToolbarExitDirection
 import androidx.compose.material3.HorizontalFloatingToolbar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -72,13 +73,20 @@ internal fun FileBrowserScreenContent(
                 HorizontalFloatingToolbar(
                     expanded = expanded,
                     floatingActionButton = {
-                        FloatingToolbarDefaults.VibrantFloatingActionButton(
+                        FloatingToolbarDefaults.StandardFloatingActionButton(
                             onClick = { expanded = !expanded },
                         ) {
-                            Icon(
-                                painter = painterResource(R.drawable.ic_add),
-                                contentDescription = "メニュー",
-                            )
+                            if (expanded) {
+                                Icon(
+                                    painter = painterResource(R.drawable.ic_arrow_back),
+                                    contentDescription = "メニューを閉じる",
+                                )
+                            } else {
+                                Icon(
+                                    painter = painterResource(R.drawable.ic_add),
+                                    contentDescription = "メニューを開く",
+                                )
+                            }
                         }
                     },
                 ) {
